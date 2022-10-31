2d28fefdf9628eb12d8175ffae5702fcf53b1e21
Juan Ignacio Yegro <nachoyegro@gmail.com>
2022-10-31 08:34:28 -0300

    websocket

---
+++ websocket/models/server.js
@@ -0,0 +1,64 @@
+import express from 'express'
+import cors from 'cors'
+
+//const { dbConnection } = require('../database/config');
+
+class Server {
+
+    constructor() {
+        this.app  = express();
+        this.port = process.env.PORT;
+
+        /*
+        this.paths = {
+            auth:       '/api/auth',
+            buscar:     '/api/buscar',
+            categorias: '/api/categorias',
+            productos:  '/api/productos',
+            usuarios:   '/api/usuarios',
+            uploads:    '/api/uploads',
+        }
+
+
+        // Conectar a base de datos
+        this.conectarDB();
+
+        // Rutas de mi aplicación
+        this.routes();
+        */
+
+        // Middlewares
+        this.middlewares();
+    }
+
+    async conectarDB() {
+        //await dbConnection();
+    }
+
+
+    middlewares() {
+
+        // CORS
+        this.app.use( cors() );
+
+        // Lectura y parseo del body
+        this.app.use( express.json() );
+
+        // Directorio Público
+        this.app.use( express.static('public') );
+
+    }
+
+    routes() {
+        //this.app.use( this.paths.auth, require('../routes/auth'));        
+    }
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port );
+        });
+    }
+
+}
+
+export default Server;
